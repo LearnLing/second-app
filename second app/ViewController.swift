cdcd2ea4a457ad803c728eeac19b4757295cd550
DianeCool的Cool蘋果 <apple@bestkuoappleever.local>
2018-03-23 19:55:32 +0800

    Initial Commit

---
+++ second app/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  second app
+//
+//  Created by DianeCool的Cool蘋果 on 2018/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
